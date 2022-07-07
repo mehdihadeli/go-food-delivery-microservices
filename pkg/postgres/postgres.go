b37a723c94d84469d051c56ed37a32e46d0e127b
mehdihadeli <mehdi.hadeli@outlook.com>
2022-07-07 13:17:43 +0430

    add sqlx for postgres

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -3,10 +3,12 @@ package postgres
 import (
 	"context"
 	"fmt"
-	"time"
-
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
+	"os"
+	"strconv"
+	"time"
 )
 
 type Config struct {
@@ -27,7 +29,7 @@ const (
 	lazyConnect       = false
 )
 
-// NewPgxConn pool
+// NewPgxConn func for connection to PostgreSQL database.
 func NewPgxConn(cfg *Config) (*pgxpool.Pool, error) {
 	ctx := context.Background()
 	var dataSourceName string
@@ -67,4 +69,50 @@ func NewPgxConn(cfg *Config) (*pgxpool.Pool, error) {
 	}
 
 	return connPool, nil
+}
+
+// NewSqlxConn func for connection to PostgreSQL database.
+func NewSqlxConn(cfg *Config) (*sqlx.DB, error) {
+	// Define database connection settings.
+	maxConn, _ := strconv.Atoi(os.Getenv("DB_MAX_CONNECTIONS"))
+	maxIdleConn, _ := strconv.Atoi(os.Getenv("DB_MAX_IDLE_CONNECTIONS"))
+	maxLifetimeConn, _ := strconv.Atoi(os.Getenv("DB_MAX_LIFETIME_CONNECTIONS"))
+
+	var dataSourceName string
+
+	if cfg.DBName == "" {
+		dataSourceName = fmt.Sprintf("host=%s port=%s user=%s password=%s",
+			cfg.Host,
+			cfg.Port,
+			cfg.User,
+			cfg.Password,
+		)
+	} else {
+		dataSourceName = fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s",
+			cfg.Host,
+			cfg.Port,
+			cfg.User,
+			cfg.DBName,
+			cfg.Password,
+		)
+	}
+
+	// Define database connection for PostgreSQL.
+	db, err := sqlx.Connect("pgx", dataSourceName)
+	if err != nil {
+		return nil, fmt.Errorf("error, not connected to database, %w", err)
+	}
+
+	// Set database connection settings.
+	db.SetMaxOpenConns(maxConn)                           // the default is 0 (unlimited)
+	db.SetMaxIdleConns(maxIdleConn)                       // defaultMaxIdleConns = 2
+	db.SetConnMaxLifetime(time.Duration(maxLifetimeConn)) // 0, connections are reused forever
+
+	// Try to ping database.
+	if err := db.Ping(); err != nil {
+		defer db.Close() // close database connection
+		return nil, fmt.Errorf("error, not sent ping to database, %w", err)
+	}
+
+	return db, nil
 }
