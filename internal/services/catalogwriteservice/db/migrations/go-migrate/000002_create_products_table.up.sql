3b3af0bc7c1af5423fc0dbe0d67f3ee88d3a6dd5
Mehdi Hadeli <mehdihadeli@gmail.com>
2024-07-22 18:29:49 +0330

    fix: 🐛fix problem in the migration for catalogswriteservice (#106)
    Fix #104

--- internal/services/catalogwriteservice/db/migrations/go-migrate/000002_create_products_table.up.sql
+++ internal/services/catalogwriteservice/db/migrations/go-migrate/000002_create_products_table.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS products
 (
-    product_id  uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id  uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     name        text,
     description text,
     price       numeric,
     created_at  timestamp with time zone,
-    updated_at  timestamp with time zone
+    updated_at  timestamp with time zone,
+    deleted_at  timestamp with time zone
 );
